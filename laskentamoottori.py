6b4f928c9078918d261e5663ef94d98ac83e45f7
joonanttila1311 <jonttu.anttila@hotmail.com>
2026-03-15 12:16:26 +0200

    Update laskentamoottori.py

--- laskentamoottori.py
+++ laskentamoottori.py
@@ -77,57 +77,83 @@ def hae_momentum(koti_nimi, vieras_nimi):
     # Palautetaan yhteinen momentum-etu kotijoukkueen näkökulmasta
     return h2h_etu + kunto_etu
 
-def laske_todennakoisyys(koti_nimi, vieras_nimi, df, koti_sp, koti_bp, koti_woba, vieras_sp, vieras_bp, vieras_woba):
+def laske_todennakoisyys(koti_nimi, vieras_nimi, koti_sp, koti_bp, koti_woba, vieras_sp, vieras_bp, vieras_woba, koti_woba_bp=None, vieras_woba_bp=None):
     """
-    Laskee ottelun lopputuloksen dynaamisilla painotuksilla.
+    Laskee ottelun lopputuloksen kahdessa dynaamisessa vaiheessa: 
+    Aloitussyöttäjä-vaihe ja Bullpen-vaihe.
     """
-    
-    # 1. Päätellään syöttäjien kesto (IP per Start)
-    # MLB-peli kestää 9 vuoroparia.
+    # TURVAVERKKO: Jos uutta BP-wobaa ei syötetä, käytetään vanhaa yhtenäistä tapaa
+    if koti_woba_bp is None: koti_woba_bp = koti_woba
+    if vieras_woba_bp is None: vieras_woba_bp = vieras_woba
+
+    # HAETAAN SYÖTTÖVUOROT (IP) - Tämä määrittää prosentit!
     koti_sp_ip = koti_sp.get("IP", 5.5)
     vieras_sp_ip = vieras_sp.get("IP", 5.5)
     
-    # 2. Lasketaan kummankin joukkueen syöttövoiman painotettu keskiarvo (xFIP)
-    # Painotus perustuu siihen, kuinka suuren osan pelistä SP hoitaa
-    def laske_tiimin_xfip(sp_data, bp_data, ip_start):
-        sp_paino = max(0.1, min(0.9, ip_start / 9.0)) # Rajoitetaan välille 10% - 90%
-        bp_paino = 1.0 - sp_paino
-        
-        # Käytetään syöttäjille aina heidän absoluuttista perustasoaan (All), 
-        # koska kätisyysetu (Platoon split) on jo laskettu sisään lyöjien wOBA-arvoihin!
-        sp_xfip = sp_data.get("xFIP_All", LIIGA_XFIP_KA)
-        bp_xfip = bp_data.get("All", LIIGA_XFIP_KA)
-        
-        return (sp_xfip * sp_paino) + (bp_xfip * bp_paino)
-
-    koti_syotto_total = laske_tiimin_xfip(koti_sp, koti_bp, koti_sp_ip)
-    vieras_syotto_total = laske_tiimin_xfip(vieras_sp, vieras_bp, vieras_sp_ip)
-
-    # 3. Voimasuhteiden vertailu (wOBA vs xFIP)
-    # Lasketaan "Edge" kummallekin joukkueelle
-    # Parempi hyökkäys (wOBA) ja huonompi vastustajan syöttö (xFIP) lisäävät etua
-    koti_etu = (koti_woba / LIIGA_WOBA_KA) - (vieras_syotto_total / LIIGA_XFIP_KA)
-    vieras_etu = (vieras_woba / LIIGA_WOBA_KA) - (koti_syotto_total / LIIGA_XFIP_KA)
+    # LASKETAAN DYNAAMISET PAINOTUKSET
+    koti_sp_paino = max(0.1, min(0.9, koti_sp_ip / 9.0))
+    koti_bp_paino = 1.0 - koti_sp_paino
     
-    # Lisätään kotikenttäetu (historiallisesti n. 3-4%)
-    koti_etu += 0.035
+    vieras_sp_paino = max(0.1, min(0.9, vieras_sp_ip / 9.0))
+    vieras_bp_paino = 1.0 - vieras_sp_paino
 
-    # LISÄTÄÄN MOMENTUM JA H2H (Noin 1% vaikutus)
+    # HAETAAN xFIP ARVOT (SP = All, BP = All)
+    koti_sp_xfip = koti_sp.get("xFIP_All", LIIGA_XFIP_KA)
+    koti_bp_xfip = koti_bp.get("All", LIIGA_XFIP_KA)
+    vieras_sp_xfip = vieras_sp.get("xFIP_All", LIIGA_XFIP_KA)
+    vieras_bp_xfip = vieras_bp.get("All", LIIGA_XFIP_KA)
+
+    # -------------------------------------------------------------
+    # 1. KOTIJOUKKUEEN HYÖKKÄYSETU (Vastassa Vieras_SP ja Vieras_BP)
+    # -------------------------------------------------------------
+    # Vaihe A: Koti hyökkää vieraan aloittajaa vastaan (Kätisyys-wOBA vs SP_xFIP)
+    koti_etu_sp = (koti_woba / LIIGA_WOBA_KA) - (vieras_sp_xfip / LIIGA_XFIP_KA)
+    
+    # Vaihe B: Koti hyökkää vieraan bullpeniä vastaan (wOBA_All vs BP_xFIP)
+    koti_etu_bp = (koti_woba_bp / LIIGA_WOBA_KA) - (vieras_bp_xfip / LIIGA_XFIP_KA)
+    
+    # Yhdistetään vastustajan kestävyyden (IP) mukaan!
+    koti_etu_perus = (koti_etu_sp * vieras_sp_paino) + (koti_etu_bp * vieras_bp_paino)
+
+    # -------------------------------------------------------------
+    # 2. VIERASJOUKKUEEN HYÖKKÄYSETU (Vastassa Koti_SP ja Koti_BP)
+    # -------------------------------------------------------------
+    # Vaihe A: Vieras hyökkää kodin aloittajaa vastaan
+    vieras_etu_sp = (vieras_woba / LIIGA_WOBA_KA) - (koti_sp_xfip / LIIGA_XFIP_KA)
+    
+    # Vaihe B: Vieras hyökkää kodin bullpeniä vastaan
+    vieras_etu_bp = (vieras_woba_bp / LIIGA_WOBA_KA) - (koti_bp_xfip / LIIGA_XFIP_KA)
+    
+    # Yhdistetään kotijoukkueen kestävyyden (IP) mukaan!
+    vieras_etu_perus = (vieras_etu_sp * koti_sp_paino) + (vieras_etu_bp * koti_bp_paino)
+
+    # -------------------------------------------------------------
+    # 3. KOTIKENTTÄ, MOMENTUM JA LOPPUTULOS
+    # -------------------------------------------------------------
+    koti_etu = koti_etu_perus + 0.035
+    
     momentum = hae_momentum(koti_nimi, vieras_nimi)
     koti_etu += momentum
 
-    # 4. Muutetaan etu todennäköisyydeksi (Logistinen funktio)
-    ero = koti_etu - vieras_etu
-    # Kerroin 5.0 on kalibroitu MLB:n varianssiin
+    ero = koti_etu - vieras_etu_perus
     koti_tod = 1 / (1 + np.exp(-5.0 * ero))
     vieras_tod = 1 - koti_tod
 
-    # 5. Juoksuodotus (O/U)
-    # Perustuu joukkueiden wOBA-tasoihin suhteessa syöttäjiin
-    perus_odotus = 8.6 # MLB keskiarvo
-    k_odotus = (perus_odotus / 2) * (koti_woba / LIIGA_WOBA_KA) * (vieras_syotto_total / LIIGA_XFIP_KA)
-    v_odotus = (perus_odotus / 2) * (vieras_woba / LIIGA_WOBA_KA) * (koti_syotto_total / LIIGA_XFIP_KA)
+    # JUOKSUODOTUS (Sama kahden vaiheen logiikka)
+    perus_odotus = 8.6 
+    k_odotus_sp = (perus_odotus / 2) * (koti_woba / LIIGA_WOBA_KA) * (vieras_sp_xfip / LIIGA_XFIP_KA)
+    k_odotus_bp = (perus_odotus / 2) * (koti_woba_bp / LIIGA_WOBA_KA) * (vieras_bp_xfip / LIIGA_XFIP_KA)
+    k_odotus = (k_odotus_sp * vieras_sp_paino) + (k_odotus_bp * vieras_bp_paino)
+
+    v_odotus_sp = (perus_odotus / 2) * (vieras_woba / LIIGA_WOBA_KA) * (koti_sp_xfip / LIIGA_XFIP_KA)
+    v_odotus_bp = (perus_odotus / 2) * (vieras_woba_bp / LIIGA_WOBA_KA) * (koti_bp_xfip / LIIGA_XFIP_KA)
+    v_odotus = (v_odotus_sp * koti_sp_paino) + (v_odotus_bp * koti_bp_paino)
+    
     total_odotus = k_odotus + v_odotus
+
+    # Lasketaan yhdistetty wOBA UI:ta varten näyttöön
+    koti_woba_total = (koti_woba * vieras_sp_paino) + (koti_woba_bp * vieras_bp_paino)
+    vieras_woba_total = (vieras_woba * koti_sp_paino) + (vieras_woba_bp * koti_bp_paino)
 
     return {
         "koti_voitto_tod": koti_tod,
@@ -135,11 +161,13 @@ def laske_todennakoisyys(koti_nimi, vieras_nimi, df, koti_sp, koti_bp, koti_woba
         "total_odotus": total_odotus,
         "k_odotus": k_odotus,
         "v_odotus": v_odotus,
-        "koti_sp_dyn": koti_sp.get("xFIP_All", LIIGA_XFIP_KA),
-        "koti_bp_dyn": koti_bp.get("vs_R", LIIGA_XFIP_KA),
-        "koti_total_xfip": koti_syotto_total,
-        "vieras_sp_dyn": vieras_sp.get("xFIP_All", LIIGA_XFIP_KA),
-        "vieras_bp_dyn": vieras_bp.get("vs_R", LIIGA_XFIP_KA),
-        "vieras_total_xfip": vieras_syotto_total,
-        "momentum_edge": momentum # Palautetaan UI:lle näytettäväksi, jos halutaan!
+        "koti_sp_dyn": koti_sp_xfip,
+        "koti_bp_dyn": koti_bp_xfip,
+        "koti_total_xfip": (koti_sp_xfip * koti_sp_paino) + (koti_bp_xfip * koti_bp_paino),
+        "vieras_sp_dyn": vieras_sp_xfip,
+        "vieras_bp_dyn": vieras_bp_xfip,
+        "vieras_total_xfip": (vieras_sp_xfip * vieras_sp_paino) + (vieras_bp_xfip * vieras_bp_paino),
+        "momentum_edge": momentum,
+        "koti_woba_total": koti_woba_total,
+        "vieras_woba_total": vieras_woba_total
     }
